28486a3535a4ee929bd23282b43e9b82eeb5057b
Liangliang Nan <liangliang.nan@gmail.com>
2019-11-15 21:37:55 +0100

    Update vec.h

--- easy3d/core/vec.h
+++ easy3d/core/vec.h
@@ -36,7 +36,7 @@
 
 namespace easy3d {
 
-
+#if 1
     template <size_t N, class T>
     class Vec {
     public:
@@ -643,8 +643,95 @@ namespace easy3d {
         return in >> v.x >> v.y >> v.z >> v.w;
     }
 
-}
 
+#else
+
+
+    template <size_t DIM, typename T> struct Vec {
+        Vec() { for (size_t i=DIM; i--; data_[i] = T()); }
+              T& operator[](const size_t i)       { assert(i<DIM); return data_[i]; }
+        const T& operator[](const size_t i) const { assert(i<DIM); return data_[i]; }
+    private:
+        T data_[DIM];
+    };
+
+    typedef Vec<2, float> vec2;
+    typedef Vec<3, float> vec3;
+    typedef Vec<3, int  > vec3i;
+    typedef Vec<4, float> vec4;
+
+    template <typename T> struct Vec<2,T> {
+        Vec() : x(T()), y(T()) {}
+        Vec(T X, T Y) : x(X), y(Y) {}
+        template <class U> Vec<2,T>(const Vec<2,U> &v);
+              T& operator[](const size_t i)       { assert(i<2); return i<=0 ? x : y; }
+        const T& operator[](const size_t i) const { assert(i<2); return i<=0 ? x : y; }
+        T x,y;
+    };
+
+    template <typename T> struct Vec<3,T> {
+        Vec() : x(T()), y(T()), z(T()) {}
+        Vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
+              T& operator[](const size_t i)       { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+        const T& operator[](const size_t i) const { assert(i<3); return i<=0 ? x : (1==i ? y : z); }
+        float norm() { return std::sqrt(x*x+y*y+z*z); }
+        Vec<3,T> & normalize(T l=1) { *this = (*this)*(l/norm()); return *this; }
+        T x,y,z;
+    };
+
+    template <typename T> struct Vec<4,T> {
+        Vec() : x(T()), y(T()), z(T()), w(T()) {}
+        Vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
+              T& operator[](const size_t i)       { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
+        const T& operator[](const size_t i) const { assert(i<4); return i<=0 ? x : (1==i ? y : (2==i ? z : w)); }
+        T x,y,z,w;
+    };
+
+    template<size_t DIM,typename T> T operator*(const Vec<DIM,T>& lhs, const Vec<DIM,T>& rhs) {
+        T ret = T();
+        for (size_t i=DIM; i--; ret+=lhs[i]*rhs[i]);
+        return ret;
+    }
+
+    template<size_t DIM,typename T>Vec<DIM,T> operator+(Vec<DIM,T> lhs, const Vec<DIM,T>& rhs) {
+        for (size_t i=DIM; i--; lhs[i]+=rhs[i]);
+        return lhs;
+    }
+
+    template<size_t DIM,typename T>Vec<DIM,T> operator-(Vec<DIM,T> lhs, const Vec<DIM,T>& rhs) {
+        for (size_t i=DIM; i--; lhs[i]-=rhs[i]);
+        return lhs;
+    }
+
+    template<size_t DIM,typename T,typename U> Vec<DIM,T> operator*(const Vec<DIM,T> &lhs, const U& rhs) {
+        Vec<DIM,T> ret;
+        for (size_t i=DIM; i--; ret[i]=lhs[i]*rhs);
+        return ret;
+    }
+
+    template<size_t DIM,typename T> Vec<DIM,T> operator-(const Vec<DIM,T> &lhs) {
+        return lhs*T(-1);
+    }
+
+
+    template<size_t DIM,typename T> T dot(const Vec<DIM,T>& lhs, const Vec<DIM,T>& rhs) {
+        T ret = T();
+        for (size_t i=DIM; i--; ret+=lhs[i]*rhs[i]);
+        return ret;
+    }
+
+    template <typename T> Vec<3,T> cross(Vec<3,T> v1, Vec<3,T> v2) {
+        return Vec<3,T>(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+    }
+
+    template <size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, const Vec<DIM,T>& v) {
+        for(unsigned int i=0; i<DIM; i++) out << v[i] << " " ;
+        return out ;
+    }
+
+#endif
+
+}
 
 #endif  // EASY3D_VEC_H
 
