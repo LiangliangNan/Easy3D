f26b44e80493852151c0977f17211df90ad9eaea
Liangliang Nan <liangliang.nan@gmail.com>
2020-05-08 12:12:51 +0200

    experiment

--- sandbox/Experiment/main.cpp
+++ sandbox/Experiment/main.cpp
@@ -3,31 +3,15 @@
 #include <easy3d/viewer/viewer.h>
 #include <easy3d/viewer/texture.h>
 #include <easy3d/fileio/image_io.h>
+#include <easy3d/core/hash.h>
 
 using namespace easy3d;
 
 int main(int argc, char **argv) {
-    // Initialize logging.
-    logging::initialize();
-
-    Viewer v;
-
-    const std::string file_name = "/Users/lnan/Projects/Easy3D/cmake-build-debug/bin/resources/colormaps/french.png";
-    int width = 0, height = 0, comp = 0;
-    std::vector<unsigned char> data;
-    // flip the image vertically, so the first pixel in the output array is the bottom left
-    bool success = ImageIO::load(file_name, data, width, height, comp, 0, true);
-    if (!success || data.empty())
-        return EXIT_FAILURE;
-
-#if 1 // discretize a gradually varying (from left to right) color image
-    const int num_colors = 16;
-    discretize_image(data, width, height, comp,num_colors );
-    ImageIO::save(file_name + "-" + std::to_string(num_colors) + ".png", data, width, height, comp);
-
-#endif
-
-
-    return v.run();
+    std::vector<float> a = {16, 0};
+    std::vector<float> b = {4, 12};
+    std::cout << "a: \n" << hash_range(a.begin(), a.end()) << std::endl;
+    std::cout << "b: \n" << hash_range(b.begin(), b.end()) << std::endl;
+    return 0;
 }
 
