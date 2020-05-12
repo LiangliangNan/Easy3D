5ac5babc0f7454759bb7856616d6ebf43abc953b
Liangliang Nan <liangliang.nan@gmail.com>
2020-05-12 15:29:25 +0200

    line spacing

--- tutorials/Tutorial_309_TextRendering/text_rendering.cpp
+++ tutorials/Tutorial_309_TextRendering/text_rendering.cpp
@@ -183,11 +183,11 @@ void TextRendering::draw() const {
             "\n'l'/'c'/'r': left/center/right align the multi-line text"
             "\n'o': switch the origin between 'upper left' and 'bottom left'"
             "\n'space': enable/disable kerning",
-            x * dpi_scaling(), y * dpi_scaling(), font_size * 1.5, TextRenderer::Align(alignment_), 0, vec3(0, 0, 0),
+            x * dpi_scaling(), y * dpi_scaling(), font_size, TextRenderer::Align(alignment_), 0, vec3(0, 0, 0),
             line_spacing_, upper_left_);
 
     // the new Y position to start; add extra space
-    y += font_height * 1.5 * (1.5 + line_spacing_) * 5;
+    y += (font_height * 1.5 + line_spacing_) * 5;
 
     float next_x = 0.0f;
     for (int i = 0; i < num_fonts; ++i) {
