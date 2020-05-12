6948d5cfd499088e0ba5fc123a98562ff57267e2
Liangliang Nan <liangliang.nan@gmail.com>
2020-05-12 22:15:07 +0200

    Update main.cpp

--- sandbox/Experiment/main.cpp
+++ sandbox/Experiment/main.cpp
@@ -7,11 +7,69 @@
 
 using namespace easy3d;
 
-int main(int argc, char **argv) {
-    std::vector<float> a = {16, 0};
-    std::vector<float> b = {4, 12};
-    std::cout << "a: \n" << hash_range(a.begin(), a.end()) << std::endl;
-    std::cout << "b: \n" << hash_range(b.begin(), b.end()) << std::endl;
-    return 0;
+
+#define STB_TRUETYPE_IMPLEMENTATION
+
+#include <3rd_party/stb/stb_truetype.h>
+
+
+#include <xmmintrin.h>
+#include <cstring>
+#include <cstdio>
+#include <chrono>
+#include <numeric>
+#include <vector>
+#include <fstream>
+
+
+int readFile(char const *filename, unsigned char **out) {
+    FILE *fp = fopen(filename, "r");
+    fseek(fp, 0, SEEK_END);
+    int size = ftell(fp);
+    rewind(fp);
+    *out = (unsigned char *) malloc(size + 1);
+    fread(*out, size, 1, fp);
+    (*out)[size] = 0;
+    fclose(fp);
+    return size;
 }
 
+int main(int argc, char **argv) {
+    std::ofstream output("easy3d.xyz");
+
+    const std::string font_file = resource::directory() + "/fonts/en_Earth-Normal.ttf";
+    const std::string text = "Easy3D";
+
+    // load font
+    unsigned char *ttf;
+    readFile(font_file.c_str(), &ttf);
+    stbtt_fontinfo font;
+    stbtt_InitFont(&font, ttf, stbtt_GetFontOffsetForIndex(ttf, 0));
+
+    float size_in_pixels = 20.0f;
+    float scale = stbtt_ScaleForPixelHeight(&font, size_in_pixels);
+    int ascent,descent,linegap;
+    stbtt_GetFontVMetrics(&font,&ascent,&descent,&linegap);
+
+
+    int offset = 0;
+    // load text
+    for (int i = 0; i < text.length(); i++) {
+        int glyph = stbtt_FindGlyphIndex(&font, text[i]);
+        stbtt_vertex *vertices;
+        int num_verts = stbtt_GetGlyphShape(&font, glyph, &vertices);
+
+//        std::cout << "\n\n" << text[i] << ": " << num_verts << std::endl;
+        for (int j=0; j<num_verts; ++j) {
+            output << vertices[j].x << " " << vertices[j].y + offset << " " << 0 << std::endl;
+        }
+
+        int advanceWidth, leftSideBearing;
+        stbtt_GetCodepointHMetrics(&font, text[i], &advanceWidth, &leftSideBearing);
+        offset += advanceWidth - leftSideBearing;
+
+        stbtt_FreeShape(&font, vertices);
+    }
+
+    return 0;
+}
