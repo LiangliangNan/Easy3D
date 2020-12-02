81ee02390e4b8e01be1de40730e50ee4b9e8394f
Liangliang Nan <liangliang.nan@gmail.com>
2020-12-02 15:00:10 +0100

    paper draft

--- sandbox/Experiment/main.cpp
+++ sandbox/Experiment/main.cpp
@@ -11,6 +11,8 @@
 #include <cstdio>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <unordered_map>
 
 #define STB_TRUETYPE_IMPLEMENTATION
 #include <3rd_party/stb/stb_truetype.h>
@@ -18,118 +20,66 @@
 
 using namespace easy3d;
 
+std::string get_question(int x, int y, bool addition) {
+    std::ostringstream ss;
+    if (x < 10)
+        ss << " " << x;
+    else
+        ss << x;
 
-int readFile(char const *filename, unsigned char **out) {
-    FILE *fp = fopen(filename, "r");
-    fseek(fp, 0, SEEK_END);
-    int size = ftell(fp);
-    rewind(fp);
-    *out = (unsigned char *) malloc(size + 1);
-    fread(*out, size, 1, fp);
-    (*out)[size] = 0;
-    fclose(fp);
-    return size;
-}
+    if (addition)
+        ss << " + ";
+    else
+        ss << " - ";
 
-
-bool extract_contours(stbtt_fontinfo *font, int codepoint, std::vector<Polygon2> &contours, unsigned short bezier_steps, float offset_x, double offset_y) {
-    int glyphIndex = stbtt_FindGlyphIndex(font, codepoint);
-    if (glyphIndex == 0) {
-        LOG(WARNING) << "given font does not support character " << string::to_string({wchar_t(codepoint)});
-        return false;
-    }
-
-    std::size_t old_num = contours.size();
-
-    stbtt_vertex *vertices = nullptr;
-    const int num_verts = stbtt_GetGlyphShape(font, glyphIndex, &vertices);
-
-    int polygonBeginIndex = 0;
-    while (polygonBeginIndex < num_verts) {
-        stbtt_vertex *nextPolygonBegin = std::find_if(vertices + polygonBeginIndex + 1, vertices + num_verts,
-                                                      [](const stbtt_vertex &p) { return p.type == STBTT_vmove; });
-        const int nextPolygonFirstIndex = std::distance(vertices, nextPolygonBegin);
-        const int currentPolygonLastIndex = nextPolygonFirstIndex - 1;
-
-        Polygon2 contour;
-        for (int vi = polygonBeginIndex; vi < currentPolygonLastIndex; ++vi) {
-            stbtt_vertex *v1 = vertices + vi;
-            stbtt_vertex *v2 = vertices + vi + 1;
-
-            const vec2 p1(v1->x + offset_x, v1->y + offset_y);
-            const vec2 p2(v2->x + offset_x, v2->y + offset_y);
-            const vec2 pc(v2->cx + offset_x, v2->cy + offset_y);
-            const vec2 pc1(v2->cx1 + offset_x, v2->cy1 + offset_y);
-
-            if (v2->type == STBTT_vline) // line
-                contour.push_back(p1);
-            else if (v2->type == STBTT_vcurve)  //quadratic Bezier
-                curve::quadratic(p1, pc, p2, contour, bezier_steps);
-            else if (v2->type == STBTT_vcubic)  //cubic Bezier
-                curve::cubic(p1, pc, pc1, p2, contour, bezier_steps);
-            else
-                LOG(ERROR) << "unrecognized contour point type";
-        }
-
-        contours.push_back(contour);
-
-        polygonBeginIndex = nextPolygonFirstIndex;
-    }
-
-    stbtt_FreeShape(font, vertices);
-
-    return contours.size() > old_num;
+    if (y < 10)
+        ss << " " << y << " = \t\t";
+    else
+        ss << y << " = \t\t";
+    return ss.str();
 }
 
 
 int main(int argc, char **argv) {
-    std::ofstream output("easy3d.xyz");
+#if 1   // For Amy
+    std::string name = "Amy";
+    int x_max = 100;
+    int y_max = 100;
+    int per_line = 4;
+#else   // For Jessie
+    std::string name = "Jessie";
+    int x_max = 20;
+    int y_max = 10;
+    int per_line = 3;
+#endif
 
-//    const std::string font_file = resource::directory() + "/fonts/en_Roboto-Medium.ttf";
-    const std::string font_file = resource::directory() + "/fonts/cn_隶体.ttf";
-    const std::wstring text = L"中国人";
 
-    // load font
-    unsigned char *ttf;
-    readFile(font_file.c_str(), &ttf);
-    int font_offset = stbtt_GetFontOffsetForIndex(ttf, 0);
-    if (font_offset != 0) {
-        LOG(ERROR) << "invalid font file";
-        return false;
+    std::vector<std::string> questions;
+    for (int x=2; x<x_max; ++x) {
+        for (int y=2; y<y_max; ++y) {
+            questions.push_back(get_question(x, y, true));
+            if (x >= y)
+                questions.push_back(get_question(x, y, false));
+        }
     }
 
-    stbtt_fontinfo font;
-    if (0 == stbtt_InitFont(&font, ttf, font_offset)) {
-        LOG(ERROR) << "init font (building font cached) failed";
-        return false;
-    }
+    std::ofstream output(name + ".txt");
+    int count = 0;
+    do {
+        int idx = rand() % (questions.size());
+        output << questions[idx];
 
-    Graph g;
+        ++count;
+        if (count % per_line == 0) {
+            output << std::endl << std::endl;
+        }
+        if (count % (per_line * 5) == 0) {
+            output << std::endl;
+        }
 
-    float offset_x = 0;
-    float offset_y = 0;
-    std::vector<Polygon2> contours;
-    for (int i = 0; i < text.length(); i++) {
-        std::vector<Polygon2> char_contours;
-        if (extract_contours(&font, text[i], char_contours, 4, offset_x, offset_y))
-            contours.insert(contours.end(), char_contours.begin(), char_contours.end());
-
-        int advanceWidth, leftSideBearing;
-        stbtt_GetCodepointHMetrics(&font, text[i], &advanceWidth, &leftSideBearing);
-        offset_x += advanceWidth - leftSideBearing;
-    }
-
-    std::size_t offset_index = 0;
-    for (const auto& contour : contours) {
-        for (const auto& p : contour)
-            g.add_vertex(vec3(p, 0.0));
-        for (std::size_t j = 0; j < contour.size(); ++j)
-            g.add_edge(Graph::Vertex(offset_index + j), Graph::Vertex(offset_index + (j + 1) % contour.size()));
-
-        offset_index += contour.size();
-    }
-
-    GraphIO::save("contours.ply", &g);
+        auto pos = std::find(questions.begin(), questions.end(), questions[idx]);
+        questions.erase(pos);
+    } while (!questions.empty());
 
     return 0;
 }
