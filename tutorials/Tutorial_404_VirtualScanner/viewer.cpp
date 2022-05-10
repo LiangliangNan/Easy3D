42c9071a4d697c85cb7d60f9d095672810b79c83
Liangliang Nan <liangliang.nan@gmail.com>
2022-05-10 23:34:38 +0200

    add virtual scanner

---
+++ tutorials/Tutorial_404_VirtualScanner/viewer.cpp
@@ -0,0 +1,104 @@
+/********************************************************************
+ * Copyright (C) 2015 Liangliang Nan <liangliang.nan@gmail.com>
+ * https://3d.bk.tudelft.nl/liangliang/
+ *
+ * This file is part of Easy3D. If it is useful in your research/work,
+ * I would be grateful if you show your appreciation by citing it:
+ * ------------------------------------------------------------------
+ *      Liangliang Nan.
+ *      Easy3D: a lightweight, easy-to-use, and efficient C++ library
+ *      for processing and rendering 3D data.
+ *      Journal of Open Source Software, 6(64), 3255, 2021.
+ * ------------------------------------------------------------------
+ *
+ * Easy3D is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3
+ * as published by the Free Software Foundation.
+ *
+ * Easy3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************/
+
+#include "viewer.h"
+#include <easy3d/core/point_cloud.h>
+#include <easy3d/renderer/camera.h>
+#include <easy3d/renderer/read_pixel.h>
+#include <easy3d/renderer/framebuffer_object.h>
+
+#include <3rd_party/glfw/include/GLFW/glfw3.h>    // for the mouse buttons
+
+
+using namespace easy3d;
+
+VirtualScanner::VirtualScanner(const std::string &title) : Viewer(title) {
+    camera()->setUpVector(vec3(0, 1, 0));
+}
+
+
+std::string VirtualScanner::usage() const {
+    return ("-------------- Virtual Scanner usage -------------- \n"
+            "Change the view using the mouse and press the Space key to perform scanning.\n"
+            "Everything (and only those) visible will be captured in a point cloud.\n"
+            "---------------------------------------------------------- \n");
+}
+
+
+bool VirtualScanner::key_press_event(int key, int modifiers) {
+    if (key == GLFW_KEY_SPACE && modifiers == 0) {
+        int fw, fh;
+        glfwGetFramebufferSize(window_, &fw, &fh);
+
+        FramebufferObject fbo(fw, fh, 0);
+        fbo.add_depth_buffer(GL_DEPTH_COMPONENT32F);
+        fbo.bind();
+        glClearDepthf(1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT);
+        draw();
+        fbo.release();
+
+        std::vector<float> depths;
+        fbo.read_depth(depths, false);
+
+        const mat4& MVP = camera()->modelViewProjectionMatrix();
+        const mat4& invMVP = inverse(MVP);
+        const int viewport[] = { 0, 0, width_, height_};
+
+        std::vector<vec3> points;
+        for (int x=0; x<width_; ++x) {
+            for (int y=0; y<height_; ++y) {
+                // NOTE: when dealing with OpenGL, we always work in the highdpi screen space
+#if defined(__APPLE__)
+                const int idx = static_cast<int>(y * dpi_scaling() * fw + x * dpi_scaling());
+#else
+                const int idx = y * fw + x;
+#endif
+                const float d = depths[idx];
+                if (d < 1.0f) {
+                    vec3 vs(x, y, d);
+                    vs.x = (float)(vs.x - viewport[0]) / (float)viewport[2] * 2.0f - 1.0f;
+                    vs.y = (float)(vs.y - viewport[1]) / (float)viewport[3] * 2.0f - 1.0f;
+                    vs.z = vs.z * 2.0f - 1.0f;
+                    points.push_back(invMVP * vs);
+                }
+            }
+        }
+
+        if (!points.empty()) {
+            PointCloud* cloud = new PointCloud;
+            for (const auto& p : points)
+                cloud->add_vertex(p);
+            add_model(cloud);
+            update();
+        }
+
+        return false;
+    }
+    else
+        return Viewer::key_press_event(key, modifiers);
+}
+
