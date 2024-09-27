40164200bb3e19a7fcac4340d3d58785a36d8b16
Liangliang Nan <liangliang.nan@gmail.com>
2024-09-27 10:30:03 +0200

    warning if wxWidgets version too low

--- easy3d/util/version.h
+++ easy3d/util/version.h
@@ -59,7 +59,7 @@ namespace easy3d {
 #define EASY3D_VERSION_NR 1020504
 
 /// Easy3D release date, in the format YYYYMMDD.
-#define EASY3D_RELEASE_DATE 20240926
+#define EASY3D_RELEASE_DATE 20240927
 
 
 #endif  // EASY3D_UTIL_VERSION_H
