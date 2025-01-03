ed688218eda8908dd7bdabc1c44e828783d776c4
Liangliang Nan <liangliang.nan@gmail.com>
2025-01-03 16:58:37 +0100

    version 2.6.0

--- easy3d/util/version.h
+++ easy3d/util/version.h
@@ -56,10 +56,10 @@ namespace easy3d {
 #define EASY3D_VERSION_STR Easy3D_VERSION_STRING
 
 /// Easy3D version number, in the format 10[MAJOR]0[MINOR]0[PATCH].
-#define EASY3D_VERSION_NR 1020504
+#define EASY3D_VERSION_NR 1020600
 
 /// Easy3D release date, in the format YYYYMMDD.
-#define EASY3D_RELEASE_DATE 20250102
+#define EASY3D_RELEASE_DATE 20250103
 
 
 #endif  // EASY3D_UTIL_VERSION_H
