0c7d03e1a272ec55148577805c2b7ac3c4fb133a
Zikuan Li <lzkcqz@163.com>
2025-01-08 17:50:09 +0800

    Fixed character issues

--- python/bindings/easy3d/algo/surface_mesh_parameterization.cpp
+++ python/bindings/easy3d/algo/surface_mesh_parameterization.cpp
@@ -16,7 +16,7 @@
 void bind_easy3d_algo_surface_mesh_parameterization(pybind11::module_& m)
 {
 	{ // easy3d::SurfaceMeshParameterization file:easy3d/algo/surface_mesh_parameterization.h line:28
-		pybind11::class_<easy3d::SurfaceMeshParameterization, std::shared_ptr<easy3d::SurfaceMeshParameterization>> cl(m, "SurfaceMeshParameterization", "A class for surface parameterization.\n \n\n\n \n It implements two parameterization methods described in the following papers:\n  - Bruno Lévy et al. Least squares conformal maps for automatic texture atlas generation. SIGGRAPH, 2002.\n  - Mathieu Desbrun et al. Intrinsic parameterizations of surface meshes. CGF, 21(3):209–218, 2002.");
+		pybind11::class_<easy3d::SurfaceMeshParameterization, std::shared_ptr<easy3d::SurfaceMeshParameterization>> cl(m, "SurfaceMeshParameterization", "A class for surface parameterization.\n \n\n\n \n It implements two parameterization methods described in the following papers:\n  - Bruno Lévy et al. Least squares conformal maps for automatic texture atlas generation. SIGGRAPH, 2002.\n  - Mathieu Desbrun et al. Intrinsic parameterizations of surface meshes. CGF, 21(3):209-218, 2002.");
 		cl.def( pybind11::init<class easy3d::SurfaceMesh *>(), pybind11::arg("mesh") );
 
 		cl.def("harmonic", [](easy3d::SurfaceMeshParameterization &o) -> void { return o.harmonic(); }, "");
